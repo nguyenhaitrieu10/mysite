54db4cc5dc5ef8c46f52d4f1a70a4f062fc7c166
Tony <haitrieu.nguyen@foodycorp.vn>
2019-09-22 21:22:14 +0700

    done toturial

--- polls/admin.py
+++ polls/admin.py
@@ -4,5 +4,24 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
